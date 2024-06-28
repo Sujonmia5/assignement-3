27b554bf58a7c1d6ca004877f01deb0e15da1d7e
Sujonmia5 <mdsujon673738@gmail.com>
2024-06-28 14:43:25 +0600

    vercell config added

--- src/app/modules/slot/route.slot.ts
+++ src/app/modules/slot/route.slot.ts
@@ -4,12 +4,13 @@ import { zodSlotValidationSchema } from "./validation.slot";
 import { slotController } from "./controller.slot";
 import { auth } from "../../middleware/authVerify";
 import { UserRole } from "../../utils/constant";
+import { AnyZodObject } from "zod";
 const route = express.Router();
 
 route.post(
   "/",
   auth(UserRole.admin),
-  requestDataValidation(zodSlotValidationSchema),
+  requestDataValidation(zodSlotValidationSchema._def.schema as AnyZodObject),
   slotController.createSlot
 );
 route.get("/availability", slotController.getAllSlots);
