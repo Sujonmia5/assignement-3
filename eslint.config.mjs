91cdaaba185bc07b940a4c9a21050096fe1eb8f5
Sujonmia5 <mdsujon673738@gmail.com>
2024-06-28 22:23:11 +0600

    problem solve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,12 @@ export default [
     ignores: ["**/node_modules", "**/dist/"],
   },
   {
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: "readonly",
+      },
+    },
   },
   {
     rules: {
